def13bba4bb85afd60f92fef92716a90ffda4a9c
Ana <anaispev@gmail.com>
2024-10-25 13:03:55 -0500

    new commit

--- ejemplo4.py
+++ ejemplo4.py
@@ -10,5 +10,41 @@ class Vehicle :
             print (f"El vehiculo {self.brand} ha sido vendido")
         else:
             print (f"El vehiculo {self.brand} no esta disponible")
+
     def check_available (self):
         return self.isAvailable
+    
+    def get_price (self):
+        return self.price
+    
+    def start_engine (self):
+        raise NotImplementedError ("Este metodo debe ser implementado por la subclase")
+    
+    def stop_engine (self):
+        raise NotImplementedError ("Este metodo debe ser implementado por la subclase")
+
+class Car (Vehicle): 
+    def start_engine (self):
+        if not self.is_available:
+            return f"El motor del coche {self.brand} esta en marcha"
+        else:
+            return f"El coche {self.brand} no esta disponible"
+            
+    def stop_engine (self):
+        if self.is_available:
+            return f"El motor del coche {self.brand} se ha detenido"
+        else:
+            return f"El coche {self.brand} no esta disponible"
+        
+class Bike (Vehicle):
+    def start_engine (self):
+        if not self.is_available:
+            return f"El motor del coche {self.brand} esta en marcha"
+        else:
+            return f"El coche {self.brand} no esta disponible"
+            
+    def stop_engine (self):
+        if self.is_available:
+            return f"El motor del coche {self.brand} se ha detenido"
+        else:
+            return f"El coche {self.brand} no esta disponible"
