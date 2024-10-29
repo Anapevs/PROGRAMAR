08fa662b44e7322b913d182f6d9406c2c81bda75
Ana <anaispev@gmail.com>
2024-10-29 13:28:54 -0500

    3338383

--- ejemplo4.py
+++ ejemplo4.py
@@ -65,3 +65,48 @@ class Customer:
         def __init__(self, name):
             self.name = name
             self.pucharsed_vehicle = []
+        def buy_vehicle(self, vehicle: Vehicle):
+            if vehicle.check_available():
+                vehicle.sell()
+                self.pucharsed_vehicle.append(vehicle)
+            else:
+                print(f'Lo siento, {vehicle.brand} no esta disponible')
+        
+        def inquire_vehicle(self,vehicle: Vehicle):
+            if vehicle.check_available():
+                availability = "Disponible"
+            else:
+                availability = "No disponible"
+            print (f"El {vehicle.brand} esta {availability} y cuesta {vehicle.get_price()}")
+
+class Dealership:
+    def __init__(self):
+        self.inventory = []
+        self.customers = []
+
+    def add_vehicles (self, vehicle: Vehicle):
+        self.inventory.append(vehicle)
+        print (f"El {vehicle.brand} ha sido añadido al inventario")
+
+    def registre_customers(self, customer: Customer):
+        self.customers.append(Customer)
+        print(f'El cliente {customer.name} ha sido añadido')
+
+    def show_available_vehicle(self):
+        print ("Vehiculos disponibles en la tienda")
+        for vehicle in self.inventory:
+            if vehicle.check_available ():
+                print(f"- {vehicle.brand} por {vehicle.get_price()}")
+
+car1 = Car ("Toyota", "Corolla", 20000)
+bike1 = Bike("Yamaha", "FH", 800)
+truck1= Truck ("Volvo", "MZ-U", 80000)
+
+customer1 = Customer ("Frank Suarez")
+
+dealer = Dealership ()
+dealer.add_vehicles (car1)
+dealer.add_vehicles (bike1)
+dealer.add_vehicles (truck1)
+
+dealer.show_available_vehicle()
