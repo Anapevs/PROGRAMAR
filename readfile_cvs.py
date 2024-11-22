b347cbee95b6f95e4809d9c5a0d29c4d79b22e7b
Ana <anaispev@gmail.com>
2024-11-22 13:15:59 -0500

    hola

--- readfile_cvs.py
+++ readfile_cvs.py
@@ -1,14 +1,15 @@
-import csv
+"""import csv
 #Leer un archivo
-"""with open('monthly_sales.csv', mode='r') as file:
+with open('monthly_sales.csv', mode='r') as file:
     csv_reader= csv.DictReader(file)
     for row in csv_reader:
-        print(row)"""
+        print(row)
 
 #Mostrar la informacion por columnas
 
 with open('monthly_sales.csv', mode='r') as file:
     csv_reader= csv.DictReader(file)
     for row in csv_reader:
-        print(f"Producto: {row['month']}, Precio: {row ['sales']}")
+        print(f"Producto: {row['month']}, Precio: {row ['sales']}")"""
         #en este ejemplo debes porner entre comillas el nombre de la columna que deseas imprimir
+ 
